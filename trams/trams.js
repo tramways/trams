e3ce1b9a0bfab6f2f09702a57e8d15dddb34ae84
Maud Nalpas <maud.nalpas@gmail.com>
2016-11-03 16:03:29 +0100

    Step 1: Graphically render tram paths Created the 1st tram object

--- trams/trams.js
+++ trams/trams.js
@@ -2,6 +2,8 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 /*alert("ctx: " + ctx);*/
 
+
+
 var lineWidth = 4;
 var lineCap = "round";
 
@@ -10,22 +12,35 @@ var tramColorB = 'green';
 var tramColorC = 'blue';
 var unifiedPeriod = 1000;
 
+ctx.lineWidth = lineWidth;
+
 var pathTramA = [
   [0, 0],
   [200, 200],
   [700, 700]
 ];
 
-function drawTramPath(tramColor){
+var tramA = {
+  path: [
+    [0, 0],
+    [200, 200],
+    [700, 700]
+  ],
+  color: 'red'
+};
+
+function drawTramPath(tram){
   ctx.beginPath();
-  ctx.lineWidth = lineWidth;
-  ctx.strokeStyle = tramColor;
-  ctx.moveTo(0,0);
-  ctx.lineTo(200, 200);
+  ctx.strokeStyle = tram.color;
+  var previousPosition = [0, 0];
+  var nextPosition = [200, 200];
+  ctx.moveTo(previousPosition[0], previousPosition[1]);//init context
+  ctx.lineTo(nextPosition[0], nextPosition[1]);//actually ove the tram
   ctx.stroke();
+  // At every period, continue the path
 }
 
-drawTramPath(tramColorA);
+drawTramPath(tramA);
 
 setTimeout(function(){ ctx.lineTo(700, 700); ctx.stroke();}, unifiedPeriod);
 
