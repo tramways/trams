d7fcd4b74d188a4cbf8329cb0fd294d90393eaf9
tramways <ma.udnalpas@gmail.com>
2016-11-06 22:33:50 +0100

    Fixed incorrect behaviour

--- js/tramRouter.js
+++ js/tramRouter.js
@@ -29,7 +29,7 @@ tramRouter = {
     if (this.nextStationIsNode(tram)){
       var competitors = this.getCompetitors(tram);
       if (this.getCompetitors(tram).length !== 0){
-        if (!this.winsPriority(tram, competitors)){
+        if (!this.checkWinsPriority(tram, competitors)){
           isAllowed = false;
         }
       }
@@ -40,7 +40,7 @@ tramRouter = {
   getCompetitors: function(tram){
     var competitors = [];
     for (var i=0 ; i<t.nbTrams ; i++){
-      if (this.nextStationIsNode(t.trams[i]) && t.trams[i]!=tram){
+      if (this.nextStationIsNode(t.trams[i]) && t.trams[i]!==tram){
         //console.log(t.trams[i].name + " competes with " + tram.name);
         competitors.push(t.trams[i]);
       }
@@ -48,80 +48,67 @@ tramRouter = {
     return competitors;
   },
 
-  winsPriority: function(tram, competitors){
+  checkWinsPriority: function(tram, competitors){
     // Also supports the case where several nodes,
     // i.e. don't leave simulatenously
-
     var competingTrams = competitors;
-    if (this.getPrioritaryTram(competitors, tram) === tram){
+    if (this.getPrioritaryTram(tram, competitors) === tram){
       return true;
     }else{
       return false;
     }
   },
 
-  getPrioritaryTram: function(competitors, tram){
-    var prioTram = this.getDefaultPrioritaryTram(competitors, tram);
-    var prioTramFromRule = this.getPrioritaryTramFromRule(competitors, tram);
-    if (prioTramFromRule !== null){
-      prioTram = prioTramFromRule;
+  getPrioritaryTram: function(tram, competitors){
+
+    var allCompetitors = competitors;
+    allCompetitors.unshift(tram);
+    var tramsWithMostPassengers = this.getTramsWithMostPassengers(allCompetitors);
+    var prioTram = undefined;
+
+    if(tramsWithMostPassengers.length === 1){
+      prioTram = tramsWithMostPassengers[0];
       userInfo.printInfo(prioTram.id + " has priority (more passengers).", false);
     }else{
-      // var nbCompetitors = competitors.length;
-      // var competitorsList = "";
-      // for (var i=0 ; i<nbCompetitors ; i++){
-      //     competitorsList += competitors[i].id + " ";
-      //     if (i !== nbCompetitors-1){
-      //       competitorsList += "and ";
-      //     }
-      // }
-      userInfo.printInfo("Same number of passengers => Using fallback priority rule (A>B>C)", true);
+      prioTram = this.getDefaultPrioritaryTram(tramsWithMostPassengers);
+      userInfo.printInfo("Same number of passengers => Using fallback priority rule (A>B>C). " + prioTram.id + " wins.", true);
     }
 
     return prioTram;
   },
 
-  getPrioritaryTramFromRule: function(competitors, tram){
-
-    var competingTrams = competitors;
-    competingTrams.unshift(tram);
-    var nbCompetitors = competingTrams.length;
-    var nbsPassengers = [];
-    var highestNbPassengers = 0;
-
-    for (var i=1 ; i<nbCompetitors ; i++){
-      nbsPassengers.push(competitors[i].nbPassengers);
-    }
-    nbsPassengers.sort(function(a, b){
-      return b-a;
-    });
-
-    for (var i=0 ; i<nbCompetitors ; i++){
-      if (competingTrams[i].nbPassengers > highestNbPassengers){
-        highestNbPassengers = competingTrams[i].nbPassengers;
-        prioTram = competingTrams[i];
+  getTramsWithMostPassengers: function(allCompetitors){
+    var nbCompetingTrams = allCompetitors.length;
+    var tramWithMostPassengers = allCompetitors[0];
+    var tramsWithMostPassengers=[];
+    var maxNbPassengers = this.getMaxNbPassengers(allCompetitors);
+    for (var i=0 ; i<nbCompetingTrams ; i++){
+      if (allCompetitors[i].nbPassengers == maxNbPassengers){
+        tramsWithMostPassengers.push(allCompetitors[i]);
       }
     }
-    if(nbsPassengers[0]>nbsPassengers[1]){
-      return prioTram;
-    }else{
-      // They are equal
-      return null;
-    }
+    return tramsWithMostPassengers;
   },
 
-  getDefaultPrioritaryTram: function(competitors, tram){
-    var competingTrams = competitors;
-    competingTrams.push(tram);
+  getMaxNbPassengers: function(trams){
+    var numbers = [];
+    var nbTrams = trams.length;
+    for (var i=0; i<nbTrams ; i++){
+      numbers.push(trams[i].nbPassengers);
+    }
+    return Math.max(...numbers);
+  },
 
-    var prioTram = competingTrams[0];
+  getDefaultPrioritaryTram: function(allCompetitors){
+
+    var prioTram = allCompetitors[0];
     var highestPrio = prioTram.defaultPriority;
-    var nbCompetitors = competitors.length;
+    var nbCompetitors = allCompetitors.length;
     for (var i=1 ; i<nbCompetitors ; i++){
-      if (competingTrams[i].defaultPriority < highestPrio) {
+      if (allCompetitors[i].defaultPriority < highestPrio) {
         // < because (Lowest prio number = Highest prio)
-        highestPrio = competingTrams[i].defaultPriority;
-        prioTram = competingTrams[i];
+        highestPrio = allCompetitors[i].defaultPriority;
+        prioTram = allCompetitors[i];
       }
     }
     return prioTram;
@@ -132,7 +119,7 @@ tramRouter = {
   },
 
   getCurrentStationIndex: function(tram){
-    // we know it's evaluated when the tram is at a station anyway
+    // Is evaluated when the tram is at a station
     var currentStationIndex = -1;
     var nbStations = tram.path.length;
     for (var i=0 ; i<nbStations ; i++){
